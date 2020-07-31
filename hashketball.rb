775c5423b0ef3501d5609d67e4b7a6b76de0e128
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-31 16:03:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,130 +1,3 @@
-# Write your code below game_hash
-def game_hash
-  {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
-        },
-        {
-          player_name: "Reggie Evans",
-          number: 30,
-          shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
-        },
-        {
-          player_name: "Brook Lopez",
-          number: 11,
-          shoe: 17,
-          points: 17,
-          rebounds: 19,
-          assists: 10,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 15
-        },
-        {
-          player_name: "Mason Plumlee",
-          number: 1,
-          shoe: 19,
-          points: 26,
-          rebounds: 11,
-          assists: 6,
-          steals: 3,
-          blocks: 8,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Jason Terry",
-          number: 31,
-          shoe: 15,
-          points: 19,
-          rebounds: 2,
-          assists: 2,
-          steals: 4,
-          blocks: 11,
-          slam_dunks: 1
-        }
-      ]
-    },
-    away: {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
-          number: 4,
-          shoe: 18,
-          points: 10,
-          rebounds: 1,
-          assists: 1,
-          steals: 2,
-          blocks: 7,
-          slam_dunks: 2
-        },
-        {
-          player_name: "Bismack Biyombo",
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 22,
-          blocks: 15,
-          slam_dunks: 10
-        },
-        {
-          player_name: "DeSagna Diop",
-          number: 2,
-          shoe: 14,
-          points: 24,
-          rebounds: 12,
-          assists: 12,
-          steals: 4,
-          blocks: 5,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Ben Gordon",
-          number: 8,
-          shoe: 15,
-          points: 33,
-          rebounds: 3,
-          assists: 2,
-          steals: 1,
-          blocks: 1,
-          slam_dunks: 0
-        },
-        {
-          player_name: "Kemba Walker",
-          number: 33,
-          shoe: 15,
-          points: 6,
-          rebounds: 12,
-          assists: 12,
-          steals: 7,
-          blocks: 5,
-          slam_dunks: 12
-        }
-      ]
-    }
-  }
-end
 require 'pry'
 
 def game_hash
@@ -132,7 +5,7 @@ def game_hash
     away: { team_name: 'Charlotte Hornets',
             colors: %w[Turquoise Purple],
             players: [
-              { player_name: "Jeff Adrien",
+              { player_name: 'Jeff Adrien',
                 number: 4,
                 shoe: 18,
                 points: 10,
@@ -316,6 +189,11 @@ def big_shoe_rebounds
   num_rebounds
 end
 
+# # Bonus Questions
+
+# # Since there are multiple bonus questions that ask me
+# # to return the name of a player with the most of some stat, I can use the following methods
+# # to DRY (don't repeat yourself) up my code.
 
 def iterate_through_players_for(name, statistic)
   game_hash.each do |_team, game_data|
@@ -351,10 +229,7 @@ def most_points_scored
 end
 
 def winning_team
-  # Set up a hash to keep track of the points scored by each team. This way, we
-  # can iterate through each player, get their points scored, and increase the
-  # count in the hash.
-
+  
   scores = { 'Brooklyn Nets' => 0, 'Charlotte Hornets' => 0 }
 
   game_hash.each do |_team, game_data|
@@ -375,4 +250,3 @@ end
 def long_name_steals_a_ton?
   player_with_most_of(:steals) == player_with_most_of(:player_name)
 end
-# Write code here
